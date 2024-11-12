bf05764b3ea395b4cacfa9949199891e0af37351
carlosvegabo <129904408+carlosvegabo@users.noreply.github.com>
2024-11-12 17:12:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,3 @@ if uploaded_file is not None:
 
 with st.container():
   st.subheader("🔍 Sube aquí el .txt de las cartas que están buscando ")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dftxt = pd.read_csv(uploaded_file)
