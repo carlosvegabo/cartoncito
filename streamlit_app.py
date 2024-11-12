ef2532ea8ed46ec0703bc0f9cba26fb049436994
carlosvegabo <129904408+carlosvegabo@users.noreply.github.com>
2024-11-12 17:12:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from io import StringIO
 import pandas as pd
 
 with st.container():
-  st.subheader("Sube aquí el .csv de tu moxfield")
+  st.subheader("💵 Sube aquí el .csv de tu moxfield")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -22,8 +22,24 @@ if uploaded_file is not None:
     st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    dfcsv = pd.read_csv(uploaded_file)
 
 with st.container():
-  st.subheader("Sube aquí el .txt de las cartas que están buscando ")
+  st.subheader("🔍 Sube aquí el .txt de las cartas que están buscando ")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dftxt = pd.read_csv(uploaded_file)
